5570a31ee5e18495d67c2a812108e2272d88343f
flowkater <chjw102@gmail.com>
2021-11-14 19:47:57 +0900

    Initial Commit

---
+++ Shared/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Shared
+//
+//  Created by flowkater on 2021/11/14.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
